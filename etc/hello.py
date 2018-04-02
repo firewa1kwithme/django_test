03405a1bf0101441ccf982caf9d0d9e8905849c0
firewa1kwithme <Markap0la>
2018-04-02 23:28:17 +0700

    fix

--- etc/hello.py
+++ etc/hello.py
@@ -1,6 +1,7 @@
 # gunicorn config
-import multiprocessing
+#import multiprocessing
 
 pythonpath = "/home/box/web"
 bind = "0.0.0.0:8080"
-workers = multiprocessing.cpu_count() * 2 + 1
+#workers = multiprocessing.cpu_count() * 2 + 1
+workers = 4
