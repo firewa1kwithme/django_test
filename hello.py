48a42c1cdf8406185d5ec460c8079635e3190ec1
firewa1kwithme <hope4arefeva@gmail.com>
2018-05-04 18:45:56 -0400

    fix gunicorn

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ def wsgi_app(environ, start_response):
 
 	status = '200 OK'
 	headers = [('Content-Type', 'text/plain')]
-	body = "\r\n".join(environ['QUERY_STRING'].split("&"))
 	start_response(status, headers)
-	return body
+	a = "\n".join(environ['QUERY_STRING'].split("&"))
+	b = [bytes(a, 'ascii')]
+	return b
